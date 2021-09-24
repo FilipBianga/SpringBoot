fdec3e86855a6da9f2b6a6d8f28d64bc78a1fdcd
Filip Bianga <filipbianga@pop-os.localdomain>
2021-09-24 14:01:10 +0200

    Fisrt app endpoint

---
+++ src/main/java/pl/bianga/zamowbook/catalog/web/CatalogController.java
@@ -0,0 +1,23 @@
+package pl.bianga.zamowbook.catalog.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.bianga.zamowbook.catalog.application.port.CatalogUseCase;
+import pl.bianga.zamowbook.catalog.domain.Book;
+
+import java.util.List;
+
+@RequestMapping("/catalog")
+@RestController
+@AllArgsConstructor
+class CatalogController {
+    private final CatalogUseCase catalog;
+
+    @GetMapping
+    public List<Book> getAll() {
+        return catalog.findAll();
+    }
+
+}
