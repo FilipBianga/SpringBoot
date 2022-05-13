79b54e1b512918c2a5a2ba3b8113b7c3df4ad843
FilipBianga <fbianga94@gmail.com>
2022-05-13 20:47:15 +0200

    Problem N+1

--- src/main/java/pl/bianga/zamowbook/catalog/db/BookJpaRepository.java
+++ src/main/java/pl/bianga/zamowbook/catalog/db/BookJpaRepository.java
@@ -1,12 +1,16 @@
 package pl.bianga.zamowbook.catalog.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.bianga.zamowbook.catalog.domain.Book;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookJpaRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.authors")
+    List<Book> findAllEager();
 
     List<Book> findByAuthors_firstNameContainsIgnoreCaseOrAuthors_lastNameContainsIgnoreCase(String firstName, String lastName);
 
