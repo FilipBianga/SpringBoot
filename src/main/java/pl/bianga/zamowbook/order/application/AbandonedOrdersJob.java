44b4846e441f1d8dd447b2458f98f0fa38d0277c
FilipBianga <fbianga94@gmail.com>
2022-06-03 14:03:59 +0200

    Test @Scheduled, nadpisywanie propertisow i beanow

--- src/main/java/pl/bianga/zamowbook/order/application/AbandonedOrdersJob.java
+++ src/main/java/pl/bianga/zamowbook/order/application/AbandonedOrdersJob.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import pl.bianga.zamowbook.clock.Clock;
 import pl.bianga.zamowbook.order.application.port.ManipulateOrderUseCase;
 import pl.bianga.zamowbook.order.db.OrderJpaRepository;
 import pl.bianga.zamowbook.order.domain.Order;
@@ -23,17 +24,19 @@ public class AbandonedOrdersJob {
     private final OrderJpaRepository repository;
     private final ManipulateOrderUseCase orderUseCase;
     private final OrdersProperties properties;
+    private final Clock clock;
+
 
     @Transactional
     @Scheduled(cron = "${app.orders.abandon-cron}")  //co 60 sekund sprawdzanie
     public void run() {
-        Duration paymantPeriod = properties.getPaymentPeriod();
-        LocalDateTime olderThan = LocalDateTime.now().minus(paymantPeriod); // co 5 minut dla produkcji lepiej sie zastanowic i dac w dniach
-        List<Order> orders = repository.findByStatusAndCreatedAtLessThanEqual(OrderStatus.NEW , olderThan);
+        Duration paymentPeriod = properties.getPaymentPeriod();
+        LocalDateTime olderThan = clock.now().minus(paymentPeriod);
+        List<Order> orders = repository.findByStatusAndCreatedAtLessThanEqual(OrderStatus.NEW, olderThan);
         log.info("Found orders to be abandoned: " + orders.size());
         orders.forEach(order -> {
-            String adminEmail = "admi@admin.pl";
-            // TODO: w sekcji security sie tym zjame
+            // TODO-Darek: naprawic w module security
+            String adminEmail = "admin@example.org";
             UpdateStatusCommand command = new UpdateStatusCommand(order.getId(), OrderStatus.ABANDONED, adminEmail);
             orderUseCase.updateOrderStatus(command);
         });
