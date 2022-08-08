ccb38f8ef0dfa2f5b19cb3ca5268f77ac02af515
FilipBianga <fbianga94@gmail.com>
2022-08-08 23:09:57 +0200

    Add Flyway and security swagger

--- src/main/java/pl/bianga/zamowbook/security/ZamowbookSecurityConfiguration.java
+++ src/main/java/pl/bianga/zamowbook/security/ZamowbookSecurityConfiguration.java
@@ -46,6 +46,7 @@ public class ZamowbookSecurityConfiguration extends WebSecurityConfigurerAdapter
                 .authorizeRequests()
                 .mvcMatchers(HttpMethod.GET, "/catalog/**", "/uploads/**", "/authors/**").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/orders", "/login", "/users").permitAll()
+                .mvcMatchers("/swagger-ui/**", "/v3/api-docs/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
